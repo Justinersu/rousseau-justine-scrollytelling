4da02836335f1adc4a484a2ee7adb6c64a514a7a
Justinersu <112107976+Justinersu@users.noreply.github.com>
2024-04-25 14:39:28 -0400

    p2.2b progression

--- script/script.js
+++ script/script.js
@@ -31,6 +31,7 @@ gsap
     }
   })
   .fromTo(`#spirale_container #spritesheet_spirale`, { scale: 1, duration: 1 }, { scale: 5, duration: 2 })
+  .to(`#chap1 .texte`, { opacity: 0, duration: 5}, '<')
 
 /*-- ---------- CHAP2 Animation du paysage -------------------- --*/
 gsap
@@ -45,3 +46,64 @@ gsap
   .fromTo(`#montagne`, { y: `-200%`, duration: 1 }, { y: 0, duration: 2 })
   .fromTo(`#chaise`, { y: `-200%`, duration: 1 }, { y: 0, duration: 2 })
   .fromTo(`#h-assit`, { y: `-200%`, duration: 1 }, { y: 0, duration: 2 })
+  .fromTo('#chap2 .texte', { opacity: 0, duration: 2}, { opacity: 1, duration: 2})
+
+/*-- ---------- CHAP3 Animation du texte -------------------- --*/
+gsap
+.timeline({
+  scrollTrigger: {
+    pin: true,
+    scrub: true,
+    markers: true,
+    trigger: `#chap3`
+  }
+})
+.fromTo(`#chap3 .texte p:nth-child(1)`, { opacity: 0, duration: 2 }, { opacity: 1, duration: 2 })
+.fromTo(`.oeil_container.un`, { opacity: 1, duration: 1 }, { opacity: 0, duration: 1 }, '<')
+.fromTo(`#chap3 .texte p:nth-child(2)`, { opacity: 0, duration: 2 }, { opacity: 1, duration: 2 })
+.fromTo(`.oeil_container.deux`, { opacity: 1, duration: 1 }, { opacity: 0, duration: 1 }, '<')
+.fromTo(`#chap3 .texte p:nth-child(3)`, { opacity: 0, duration: 2 }, { opacity: 1, duration: 2 })
+.fromTo(`.oeil_container.trois`, { opacity: 1, duration: 1 }, { opacity: 0, duration: 1 }, '<')
+.to(`#chap3 .texte`, { opacity: 0, duration: 2 }, '+=2')
+
+/*-- ---------- CHAP4 Animation des visages -------------------- --*/
+gsap
+.timeline({
+  scrollTrigger: {
+    pin: true,
+    scrub: true,
+    markers: true,
+    trigger: `#chap4`
+  }
+})
+.from(`#visage2`, { opacity: 0, duration: 1 })
+.from(`#visage3`, { opacity: 0, duration: 1 })
+.from(`#visage4`, { opacity: 0, duration: 1 })
+.from(`#visage5`, { opacity: 0, duration: 1 })
+.from(`#visage6`, { opacity: 0, duration: 1 })
+.from(`#visage7`, { opacity: 0, duration: 1 })
+.from(`#visage8`, { opacity: 0, duration: 1 })
+.from(`#visage9`, { opacity: 0, duration: 1 })
+.from(`#visage10`, { opacity: 0, duration: 1 })
+.from(`#visage11`, { opacity: 0, duration: 1 })
+.from(`#visage12`, { opacity: 0, duration: 1 })
+.from(`#visage13`, { opacity: 0, duration: 1 })
+.from(`#visage14`, { opacity: 0, duration: 1 })
+.from(`#visage15`, { opacity: 0, duration: 1 })
+.from(`#visage16`, { opacity: 0, duration: 1 })
+.from(`#visage17`, { opacity: 0, duration: 1 })
+.from(`#visage18`, { opacity: 0, duration: 1 })
+.from(`#visage19`, { opacity: 0, duration: 1 })
+.from(`#visage20`, { opacity: 0, duration: 1 })
+.from(`#chap4 .texte`, { opacity: 0, duration: 3 })
+
+/*-- ---------- CHAP4 Animation de l'homme qui tombe -------------------- --*/
+gsap
+.timeline({
+  scrollTrigger: {
+    pin: true,
+    scrub: true,
+    markers: true,
+    trigger: `#chap5`
+  }
+})
