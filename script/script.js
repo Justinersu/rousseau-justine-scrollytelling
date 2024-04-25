7e116c2ba86b3f84354755e7a6fe30073f285a47
Justinersu <112107976+Justinersu@users.noreply.github.com>
2024-04-25 11:56:02 -0400

    P2.2b progression

--- script/script.js
+++ script/script.js
@@ -1,4 +1,4 @@
-/*----Scroll Event----*/
+/*-- ---------- Scroll Event -------------------- --*/
 let minuteur;
 let body = document.querySelector("body");
 
@@ -11,9 +11,37 @@ window.addEventListener('scroll', () => {
   }, 100);
 });
 
-/*----Animation du bouton defiler----*/
+/*-- ---------- Animation du bouton defiler -------------------- --*/
 gsap.set("#defiler", { opacity: 1 });
 gsap.to("#defiler", { opacity: 0, repeat: -1, duration: 2 });
 
 gsap.set("#defiler", { y: 0 });
 gsap.to("#defiler", { y: "30px", repeat: -1, duration: 2 });
+
+gsap.registerPlugin(ScrollTrigger);
+
+/*-- ---------- CHAP1 Animation de la spirale -------------------- --*/
+gsap
+  .timeline({
+    scrollTrigger: {
+      pin: true,
+      scrub: true,
+      markers: true,
+      trigger: `#chap1`
+    }
+  })
+  .fromTo(`#spirale_container #spritesheet_spirale`, { scale: 1, duration: 1 }, { scale: 5, duration: 2 })
+
+/*-- ---------- CHAP2 Animation du paysage -------------------- --*/
+gsap
+  .timeline({
+    scrollTrigger: {
+      pin: true,
+      scrub: true,
+      markers: true,
+      trigger: `#chap2`
+    }
+  })
+  .fromTo(`#montagne`, { y: `-200%`, duration: 1 }, { y: 0, duration: 2 })
+  .fromTo(`#chaise`, { y: `-200%`, duration: 1 }, { y: 0, duration: 2 })
+  .fromTo(`#h-assit`, { y: `-200%`, duration: 1 }, { y: 0, duration: 2 })
